4ee7d7c4f71cde9e82b28921f2d06cd77bb65b3a
Artem Alexeyenko <ala@sitecore.net>
2025-01-23 15:56:50 -0500

    attempt fix 2

--- headapps/nextjs-starter/src/lib/render-editing-middleware.ts
+++ headapps/nextjs-starter/src/lib/render-editing-middleware.ts
@@ -94,7 +94,11 @@ export class ChromesHandler extends RenderMiddlewareBase {
     this.editingDataService = config?.editingDataService ?? editingDataService;
     this.dataFetcher =
       config?.dataFetcher ??
-      new NativeDataFetcher({ debugger: debug.editing, credentials: 'include' });
+      new NativeDataFetcher({
+        debugger: debug.editing,
+        credentials: 'include',
+        headers: { Accept: '*/*' },
+      });
     this.resolvePageUrl = config?.resolvePageUrl ?? this.defaultResolvePageUrl;
     this.resolveServerUrl = config?.resolveServerUrl ?? this.defaultResolveServerUrl;
   }
